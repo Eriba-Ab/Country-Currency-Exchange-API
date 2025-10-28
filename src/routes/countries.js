3058958a5d3fa5ab66d7f263524cf4903912783d
mee-farms <eribainc@gmail.com>
2025-10-28 16:03:15 +0100

    fix: remove duplicate route definition for /countries/image in countries.js

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -8,13 +8,13 @@ router.post('/refresh', controller.postRefresh);
 // GET /countries
 router.get('/', controller.getCountries);
 
+// GET /countries/image
+router.get('/image', controller.getImage);
+
 // GET /countries/:name
 router.get('/:name', controller.getCountryByName);
 
 // DELETE /countries/:name
 router.delete('/:name', controller.deleteCountry);
-
-// GET /countries/image
-router.get('/image', controller.getImage);
 
 module.exports = router;
